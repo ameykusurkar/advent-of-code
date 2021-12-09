1f588b1fbe51e10c9afddef2504090b9e3b7962f
Amey Kusurkar <amey1000@gmail.com>
2021-12-10 00:57:33 +0800

    Day 2 part 2

--- 2021/day02/solution.hs
+++ 2021/day02/solution.hs
@@ -1,9 +1,9 @@
 main = do
   movements <- fmap parse getContents
   print $ solve1 movements
+  print $ solve2 movements
 
 data Movement = Up Int | Down Int | Forward Int
-type Position = (Int, Int) -- (Horizontal, Depth)
 
 parse :: String -> [Movement]
 parse = (map parseLine) . lines
@@ -17,7 +17,17 @@ solve1 ms = horizontal * depth
   where
     (horizontal, depth) = foldl moveStep (0, 0) ms
 
-moveStep :: Position -> Movement -> Position
+solve2 :: [Movement] -> Int
+solve2 ms = horizontal * depth
+  where
+    (horizontal, depth, _aim) = foldl aimStep (0, 0, 0) ms
+
+moveStep :: (Int, Int) -> Movement -> (Int, Int)
 moveStep (h, d) (Up x)      = (h, d - x)
 moveStep (h, d) (Down x)    = (h, d + x)
 moveStep (h, d) (Forward x) = (h + x, d)
+
+aimStep :: (Int, Int, Int) -> Movement -> (Int, Int, Int)
+aimStep (h, d, a) (Up x)      = (h, d, a - x)
+aimStep (h, d, a) (Down x)    = (h, d, a + x)
+aimStep (h, d, a) (Forward x) = (h + x, d + a * x, a)
