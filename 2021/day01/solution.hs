6df0bf5a52abc45c38e1e0fc825a91150d184fdd
Amey Kusurkar <amey1000@gmail.com>
2021-12-09 23:34:53 +0800

    Day 1 part 1

--- 2021/day01/solution.hs
+++ 2021/day01/solution.hs
@@ -1,1 +1,16 @@
-main = interact id
+main = interact (show . solve . parse)
+
+parse :: String -> [Int]
+parse = (map read) . lines
+
+solve :: [Int] -> Int
+solve xs = count isIncreasing (pairwise xs)
+
+pairwise :: [a] -> [(a, a)]
+pairwise xs = zip xs (tail xs)
+
+isIncreasing :: (Int, Int) -> Bool
+isIncreasing (a, b) = b > a
+
+count :: (a -> Bool) -> [a] -> Int
+count pred = length . filter pred
