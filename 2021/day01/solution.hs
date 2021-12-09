56415db8aaf432405b1b39ffd958848bc3d84e06
Amey Kusurkar <amey1000@gmail.com>
2021-12-10 00:07:54 +0800

    Day 1 part 2

--- 2021/day01/solution.hs
+++ 2021/day01/solution.hs
@@ -1,13 +1,23 @@
-main = interact (show . solve . parse)
+main = do
+  nums <- fmap parse getContents
+  print $ solve1 nums
+  print $ solve2 nums
 
 parse :: String -> [Int]
 parse = (map read) . lines
 
-solve :: [Int] -> Int
-solve xs = count isIncreasing (pairwise xs)
+solve1 :: [Int] -> Int
+solve1 xs = count isIncreasing (pairwise xs)
+
+solve2 :: [Int] -> Int
+solve2 xs = count isIncreasing (pairwise triples)
+  where triples = map (\(a, b, c) -> a + b + c) (threewise xs)
 
 pairwise :: [a] -> [(a, a)]
 pairwise xs = zip xs (tail xs)
+
+threewise :: [a] -> [(a, a, a)]
+threewise (x:xs) = zip3 (x:xs) xs (tail xs)
 
 isIncreasing :: (Int, Int) -> Bool
 isIncreasing (a, b) = b > a
