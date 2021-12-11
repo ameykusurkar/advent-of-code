a6df1979206340798a0c10827773bb43d04da22a
Amey Kusurkar <amey1000@gmail.com>
2021-12-11 20:39:03 +0800

    Day 3 part 2

--- 2021/day03/solution.hs
+++ 2021/day03/solution.hs
@@ -5,6 +5,7 @@ import Data.List
 main = do
   strs <- fmap lines getContents
   print $ solve1 strs
+  print $ solve2 strs
 
 parseBinary :: String -> Int
 parseBinary str = sum $ zipWith shift bits indexes
@@ -22,8 +23,16 @@ flipC :: Char -> Char
 flipC = intToDigit . (1-) . fromEnum . (== '1')
 
 moreCommonChar :: [Char] -> Char
-moreCommonChar cs = boolToChar $ (count (== '1') cs) > (count (== '0') cs)
-  where boolToChar b = if b then '1' else '0' 
+moreCommonChar = boolToChar . mostCommonBit . map (== '1')
+
+boolToChar :: Bool -> Char
+boolToChar b = if b then '1' else '0'
+
+mostCommonBit :: [Bool] -> Bool
+mostCommonBit bs = (count id bs) >= (count (not . id) bs)
+
+leastCommonBit :: [Bool] -> Bool
+leastCommonBit = not . mostCommonBit
 
 -- Gets the nth bit of `x`, where the 0th bit is the LSB
 getBit :: Int -> Int -> Int
@@ -32,3 +41,30 @@ getBit n x = if (nthOne .&. x) > 0 then 1 else 0
 
 count :: (a -> Bool) -> [a] -> Int
 count pred = length . filter pred
+
+solve2 :: [String] -> Int
+solve2 str = foo * bar
+  where foo = parseBinary $ map boolToChar $ winningBa mostCommonBit bas
+        bar = parseBinary $ map boolToChar $ winningBa leastCommonBit bas
+        bas = transpose $ map (map charToBool) str
+
+charToBool :: Char -> Bool
+charToBool = (== '1')
+
+type BitArray = [Bool]
+
+-- Taking an array of bit arrays, returns the bit array where each bit "wins"
+-- when comparing against the the other bits at the corresponding position 
+winningBa :: (BitArray -> Bool) -> [BitArray] -> BitArray
+winningBa _ [] = []
+winningBa _ ((b:[]):bas) = b : (map head bas)
+winningBa winCond (ba:bas) = winBit : (winningBa winCond winningBas)
+  where winningBas = map (filterTrues hasWon) bas
+        hasWon = map (== winBit) ba
+        winBit = winCond ba
+
+filterTrues :: [Bool] -> [a] -> [a]
+filterTrues trues = map snd. filter fst . zip trues
+
+shouldPick :: (a -> Bool) -> [a] -> [Bool]
+shouldPick pred xs = map pred xs
