d1a18a6ce736c784f0e2becff5fddf71455587e6
Amey Kusurkar <amey1000@gmail.com>
2021-12-12 00:17:56 +0800

    Day 4 part 2

--- 2021/day04/solution.hs
+++ 2021/day04/solution.hs
@@ -5,7 +5,8 @@ main = do
   nums <- fmap parseNums getLine
   getLine
   boards <- fmap parseBoards getContents
-  print $ solve nums boards
+  print $ solve1 nums boards
+  print $ solve2 nums boards
 
 parseNums:: String -> [Int]
 parseNums = map read . splitOn ','
@@ -16,11 +17,20 @@ parseBoards :: String -> [Board]
 parseBoards = map buildBoard . splitOn "" . lines
   where buildBoard = map (map (\x -> (x, False))) . map (map read) . map words
 
-solve :: [Int] -> [Board] -> Int
-solve (n:ns) boards = case find didWin boards' of
+solve1 :: [Int] -> [Board] -> Int
+solve1 (n:ns) boards = case find didWin boards' of
   Just board -> n * sum (unmarked board)
-  Nothing -> solve ns boards'
+  Nothing -> solve1 ns boards'
   where boards' = map (bingo n) boards
+
+solve2 :: [Int] -> [Board] -> Int
+solve2 (n:ns) boards
+  | gameOver boards' = n * sum (unmarked (bingo n lastToWin))
+  | otherwise = solve2 ns boards'
+  where boards' = map (bingo n) boards
+        gameOver = (== 0) . length . yetToWin
+        yetToWin = filter (not . didWin)
+        lastToWin = head (yetToWin boards)
 
 didWin :: Board -> Bool
 didWin rows = any allTrue rows || any allTrue (transpose rows)
