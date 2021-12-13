d954cb78ee6401641f6e32db261028f6fe741d71
Amey Kusurkar <amey1000@gmail.com>
2021-12-13 09:38:08 +0800

    Some improvements to old days

--- 2021/day01/solution.hs
+++ 2021/day01/solution.hs
@@ -7,20 +7,20 @@ parse :: String -> [Int]
 parse = (map read) . lines
 
 solve1 :: [Int] -> Int
-solve1 xs = count isIncreasing (pairwise xs)
+solve1 = countIncreasing
 
 solve2 :: [Int] -> Int
-solve2 xs = count isIncreasing (pairwise triples)
-  where triples = map (\(a, b, c) -> a + b + c) (threewise xs)
+solve2 = countIncreasing . map sum . threewise
+  where sum (a, b, c) = a + b + c
+
+countIncreasing :: [Int] -> Int
+countIncreasing xs = count (uncurry (<)) (pairwise xs)
 
 pairwise :: [a] -> [(a, a)]
 pairwise xs = zip xs (tail xs)
 
 threewise :: [a] -> [(a, a, a)]
 threewise (x:xs) = zip3 (x:xs) xs (tail xs)
-
-isIncreasing :: (Int, Int) -> Bool
-isIncreasing (a, b) = b > a
 
 count :: (a -> Bool) -> [a] -> Int
 count pred = length . filter pred
--- 2021/day02/solution.hs
+++ 2021/day02/solution.hs
@@ -1,27 +1,23 @@
 main = do
-  movements <- fmap parse getContents
+  movements <- fmap (map parseLine . lines)  getContents
   print $ solve1 movements
   print $ solve2 movements
 
 data Movement = Up Int | Down Int | Forward Int
 
-parse :: String -> [Movement]
-parse = (map parseLine) . lines
-  where
-    parseLine line = case words line of
-      ["up", n] -> Up (read n)
-      ["down", n] -> Down (read n)
-      ["forward", n] -> Forward (read n)
+parseLine :: String -> Movement
+parseLine line = case words line of
+  ["up", n] -> Up (read n)
+  ["down", n] -> Down (read n)
+  ["forward", n] -> Forward (read n)
 
 solve1 :: [Movement] -> Int
 solve1 ms = horizontal * depth
-  where
-    (horizontal, depth) = foldl moveStep (0, 0) ms
+  where (horizontal, depth) = foldl moveStep (0, 0) ms
 
 solve2 :: [Movement] -> Int
 solve2 ms = horizontal * depth
-  where
-    (horizontal, depth, _aim) = foldl aimStep (0, 0, 0) ms
+  where (horizontal, depth, _aim) = foldl aimStep (0, 0, 0) ms
 
 moveStep :: (Int, Int) -> Movement -> (Int, Int)
 moveStep (h, d) (Up x)      = (h, d - x)
