6be56c02d782a4f913c726492f255e6b71f029c5
Amey Kusurkar <amey1000@gmail.com>
2021-12-17 14:09:18 +0800

    Day 12 part 2

--- 2021/day12/solution.hs
+++ 2021/day12/solution.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE TupleSections #-}
+
 import Data.Char
 import Data.Maybe
+import qualified Data.Map as M
 
 data Edge = Edge { getFrom, getTo :: Node } deriving Show
-data Node = Start | End | Big String | Small String deriving Eq
+data Node = Start | End | Big String | Small String deriving (Ord, Eq)
 type Path = [Node]
 
 instance Show Node where
@@ -15,7 +18,8 @@ main = do
   rawEdges <- fmap (map parseLine . lines) getContents
   let edges = concatMap buildEdges rawEdges
   let start = [Start]
-  print $ solve1 edges start
+  print $ solve allowSmallOnce edges start
+  print $ solve allowOneSmallTwice edges start
 
 buildEdges :: Edge -> [Edge]
 buildEdges e@(Edge Start _) = [e]
@@ -30,16 +34,33 @@ parseLine str = Edge (parseNode n1) (parseNode n2)
         parseNode "end" = End
         parseNode str = if isUpper (head str) then Big str else Small str
 
-solve1 :: [Edge] -> Path -> Int
-solve1 edges (End:_) = 1
-solve1 edges path = foldl f 0 nexts
-  where f acc n = acc + solve1 edges (n:path)
-        nexts = mapMaybe (nextNode path) edges
+type SmallGuard = Node -> Path -> Bool
 
-nextNode :: Path -> Edge -> Maybe Node
-nextNode (n:ns) (Edge n1 n2@(Small s)) = if n == n1 && notElem n2 ns then Just n2 else Nothing
-nextNode (n:ns) (Edge n1 n2) = if n == n1 then Just n2 else Nothing 
-nextNode _       _  = Nothing
+solve :: SmallGuard -> [Edge] -> Path -> Int
+solve sg edges (End:_) = 1
+solve sg edges path = foldl f 0 nexts
+  where f acc n = acc + solve sg edges (n:path)
+        nexts = map getTo $ filter (canExtend sg path) edges
+
+canExtend :: SmallGuard -> Path -> Edge -> Bool
+canExtend sg (n:ns) (Edge n1 n2@(Small s)) = n == n1 && sg n2 (n:ns)
+canExtend _  (n:ns) (Edge n1 n2)           = n == n1
+canExtend _  _       _                     = False
+
+allowSmallOnce :: SmallGuard
+allowSmallOnce = notElem
+
+allowOneSmallTwice :: SmallGuard
+allowOneSmallTwice sml path = count sml == 0 || null twos
+  where count s = M.findWithDefault 0 s m
+        twos = filter (\s -> count s == 2) smalls
+        m = buildCounter smalls
+        smalls = filter isSmall path
+        isSmall (Small _) = True
+        isSmall _         = False
+
+buildCounter :: Ord a => [a] -> M.Map a Int
+buildCounter = M.fromListWith (+) . map (,1)
 
 splitOn :: Eq a => a -> [a] -> [[a]]
 splitOn c str = case break (== c) str of
