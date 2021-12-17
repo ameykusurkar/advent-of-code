1a2523b5585f13b693f3440f5f1f04cf0aeaf6cd
Amey Kusurkar <amey1000@gmail.com>
2021-12-17 10:03:34 +0800

    Day 12 part 1

--- 2021/day12/solution.hs
+++ 2021/day12/solution.hs
@@ -1,16 +1,27 @@
--- {-# LANGUAGE DeriveFoldable, DeriveFunctor, TupleSections #-}
-
 import Data.Char
-import Data.List
+import Data.Maybe
 
 data Edge = Edge { getFrom, getTo :: Node } deriving Show
-data Node = Start | End | Big String | Small String deriving (Eq, Show)
+data Node = Start | End | Big String | Small String deriving Eq
 type Path = [Node]
 
+instance Show Node where
+  show Start = "start"
+  show End = "end"
+  show (Big str) = str
+  show (Small str) = str
+
 main = do
-  edges <- fmap (map parseLine . lines) getContents
-  let f = solve1 edges
-  print $ [[Start]] >>= f >>= f
+  rawEdges <- fmap (map parseLine . lines) getContents
+  let edges = concatMap buildEdges rawEdges
+  let start = [Start]
+  print $ solve1 edges start
+
+buildEdges :: Edge -> [Edge]
+buildEdges e@(Edge Start _) = [e]
+buildEdges e@(Edge n Start) = [Edge Start n]
+buildEdges e@(Edge _ End) = [e]
+buildEdges (Edge n1 n2) = [Edge n1 n2, Edge n2 n1]
 
 parseLine :: String -> Edge
 parseLine str = Edge (parseNode n1) (parseNode n2)
@@ -19,13 +30,16 @@ parseLine str = Edge (parseNode n1) (parseNode n2)
         parseNode "end" = End
         parseNode str = if isUpper (head str) then Big str else Small str
 
-solve1 :: [Edge] -> Path -> [Path]
-solve1 = extend
+solve1 :: [Edge] -> Path -> Int
+solve1 edges (End:_) = 1
+solve1 edges path = foldl f 0 nexts
+  where f acc n = acc + solve1 edges (n:path)
+        nexts = mapMaybe (nextNode path) edges
 
-extend :: [Edge] -> Path -> [Path]
-extend edges nodes = map (:nodes) (cands edges)
-  where cands = map getTo . filter (startsWith (head nodes))
-        startsWith n (Edge n1 n2) = n == n1
+nextNode :: Path -> Edge -> Maybe Node
+nextNode (n:ns) (Edge n1 n2@(Small s)) = if n == n1 && notElem n2 ns then Just n2 else Nothing
+nextNode (n:ns) (Edge n1 n2) = if n == n1 then Just n2 else Nothing 
+nextNode _       _  = Nothing
 
 splitOn :: Eq a => a -> [a] -> [[a]]
 splitOn c str = case break (== c) str of
