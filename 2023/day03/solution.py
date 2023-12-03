76abdfe680d48912695236fa47aafb8e38a939c9
Amey Kusurkar <amey1000@gmail.com>
2023-12-03 10:44:15 +0000

    Solve 2023 day3 part 2

--- 2023/day03/solution.py
+++ 2023/day03/solution.py
@@ -1,26 +1,44 @@
 import numpy as np
-import sys
-
-lines = sys.stdin.read().split()
-parsed = np.array([list(l) for l in lines])
-padded = np.pad(parsed, (1, 1), constant_values='.')
+from collections import defaultdict
+from sys import stdin
+from math import prod
 
 D = '0123456789'
 
-def has_symbols(area: np.ndarray) -> bool:
+def numbers(grid: np.ndarray):
+    rows, cols = grid.shape
+    for i in range(1, rows):
+        for j in range(1, cols):
+            if grid[i, j] in D and grid[i, j-1] not in D:
+                end_j = j + next(idx for (idx, n) in enumerate(grid[i, j:]) if n not in D)
+                area = grid[i-1:i+2, j-1:end_j+1]
+                val = int(''.join(grid[i, j:end_j]))
+                yield(i, j, area, val)
+
+def has_sym(area: np.ndarray) -> bool:
     not_syms = D + '.'
     return any(c for c in area.ravel() if c not in not_syms)
 
-part1 = 0
+def find_gears(area: np.ndarray) -> list[tuple[int, int]]:
+    rs, cs = area.shape
+    gear_locations = []
+    for i in range(0, rs):
+        for j in range(0, cs):
+            if area[i, j] == '*':
+                gear_locations.append((i, j))
+    return gear_locations
 
-rows, cols = padded.shape
-for i in range(1, rows):
-    for j in range(1, cols):
-        if padded[i, j] in D and padded[i, j-1] not in D:
-            end_j = next(idx for (idx, n) in enumerate(padded[i, j:]) if n not in D)
-            area = padded[i-1:i+2, j-1:j+1+end_j]
-            if has_symbols(area):
-                arr = padded[i, j:j+end_j]
-                part1 += int(''.join(arr))
+lines = stdin.read().split()
+parsed = np.array([list(l) for l in lines])
+grid = np.pad(parsed, (1, 1), constant_values='.')
 
+part1 = sum(val for _, _, area, val in numbers(grid) if has_sym(area))
 print(part1)
+
+gears = defaultdict(list)
+for i, j, area, val in numbers(grid):
+    for (gi, gj) in find_gears(area):
+        gears[(i + gi - 1, j + gj - 1)].append(val)
+
+part2 = sum(prod(vals) for vals in gears.values() if len(vals) > 1)
+print(part2)
