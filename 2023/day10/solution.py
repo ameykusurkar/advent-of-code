5018d2eb9621ccf5054f979fe11a001296be24aa
Amey Kusurkar <amey1000@gmail.com>
2023-12-10 18:05:33 +0000

    Solve 2023 day 10 part 2

--- 2023/day10/solution.py
+++ 2023/day10/solution.py
@@ -1,4 +1,3 @@
-from sys import stdin
 from math import ceil
 import numpy as np
 
@@ -6,19 +5,24 @@ MOVES = {
   # Arranged based on multiples of i to rotate by
   #          -i    0    i  
   (-1,  0): ['F', '|', '7'], # up
-  ( 1,  0): ['J', '|', 'L'], # down
   ( 0, -1): ['L', '-', 'F'], # left
+  ( 1,  0): ['J', '|', 'L'], # down
   ( 0,  1): ['7', '-', 'J'], # right
 }
+
+OFFSETS = set([
+              (-1, 0),
+    ( 0, -1),          ( 0, 1),
+              ( 1, 0),
+])
 
 def rotate(point: tuple[int, int], n: int) -> tuple[int, int]:
     rotated = complex(point[1], -point[0]) * (1j ** n)
     return -int(rotated.imag), int(rotated.real)
 
-def is_within(target: tuple[int, int], pos: tuple[int, int]) -> bool:
-    ti, tj = target
-    pi, pj = pos
-    return 0 <= pi < ti and 0 <= pj < tj
+def is_within(grid: np.ndarray, pos: tuple[int, int]) -> bool:
+    target = grid.shape
+    return 0 <= pos[0] < target[0] and 0 <= pos[1] < target[1]
 
 def find_start_dir(grid, pos) -> tuple[int, int]:
     for dir in MOVES.keys():
@@ -29,36 +33,71 @@ def find_start_dir(grid, pos) -> tuple[int, int]:
 
 def move(grid: np.ndarray, pos: tuple[int, int], dir: tuple[int, int]):
     new_pos = (pos[0] + dir[0], pos[1] + dir[1])
-    if not is_within(grid.shape, new_pos):
+    if not is_within(grid, new_pos):
         return None
     pipe = grid[new_pos] 
     if pipe == "S":
         return "loop"
-    options = MOVES[dir]
     try:
-        rotations = options.index(pipe) - 1
+        i = MOVES[dir].index(pipe) - 1
     except:
         return None
-    new_dir = rotate(dir, rotations)
+    new_dir = rotate(dir, i)
     return new_pos, new_dir
 
-lines = stdin.read().split()
+def side(grid: np.ndarray, pos: tuple[int, int], dir: tuple[int, int], rot):
+    new_dir = rotate(dir, rot)
+    new_pos = (pos[0] + new_dir[0], pos[1] + new_dir[1])
+    if not is_within(grid, new_pos):
+        return None
+    return new_pos, grid[new_pos]
+
+def floodfill(grid, to_visit: set[tuple[int, int]], main_loop) -> set[tuple[int, int]]:
+    visited = set()
+    while to_visit:
+        pos = to_visit.pop()
+        if pos in visited:
+            continue
+        visited.add(pos)
+        for dir in OFFSETS: 
+            new_pos = (pos[0] + dir[0], pos[1] + dir[1])
+            if is_within(grid, new_pos) and (new_pos not in main_loop):
+                to_visit.add(new_pos)
+    return visited
+
+lines = open(0).read().split()
 grid = np.array([list(l) for l in lines])
 
 startI, startJ = np.where(grid == 'S')
 start_pos = startI[0], startJ[0]
 start_dir = find_start_dir(grid, start_pos)
 
+loop = set()
+inner_left, inner_right = set(), set()
 current_pos, current_dir = start_pos, start_dir
-count = 0
 while True:
-    count += 1
+    loop.add(current_pos)
+    old_dir = current_dir
     match move(grid, current_pos, current_dir):
         case None:
             raise Exception("What happened")
         case "loop":
             break
-        case next:
-            current_pos, current_dir = next
+        case current_pos, current_dir:
+            for side_dir in [old_dir, current_dir]:
+                right = side(grid, current_pos, side_dir, -1)
+                if right:
+                    right_pos, right_pipe = right
+                    inner_right.add(right_pos)
+                left = side(grid, current_pos, side_dir, 1)
+                if left:
+                    left_pos, left_pipe = left
+                    inner_left.add(left_pos)
 
-print(ceil(count/2))
+print(ceil(len(loop)/2))
+
+left_fill = floodfill(grid, inner_left - loop, loop)
+right_fill = floodfill(grid, inner_right - loop, loop)
+
+print(len(left_fill))
+print(len(right_fill))
