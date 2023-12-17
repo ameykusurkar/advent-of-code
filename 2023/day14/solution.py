95ace3775c12bb5967e1a64f7fb97f8633515c72
Amey Kusurkar <amey1000@gmail.com>
2023-12-17 23:15:18 +0000

    Make day 14 faster

--- 2023/day14/solution.py
+++ 2023/day14/solution.py
@@ -1,22 +1,50 @@
 import numpy as np
+from time import time
+
+def north(grid, j):
+    start = time()
+    rows, _ = grid.shape
+    start, end = 0, 0
+    while start < rows - 1:
+        while grid[start, j] != '.' and start < rows - 2:
+            start += 1
+        if start == rows - 1:
+            return
+        # start points to '.'
+        end = start + 1
+        while end < rows and grid[end, j] == '.' and end < rows:
+            end += 1
+        if end == rows:
+            return
+        if grid[end, j] == '#':
+            start = end + 1
+            continue
+        o_start = end
+        # o_start points to first 'O'
+        while end < rows and grid[end, j] == 'O':
+            end += 1
+        # end points to first non-'O'
+        for x in range(o_start, end):
+            grid[x, j] = '.'
+        num_os = end - o_start
+        new_start = start + num_os
+        while start < new_start:
+            grid[start, j] = 'O'
+            start += 1
+
+def count(grid):
+    rows, cols = grid.shape
+    total = 0
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i, j] == 'O':
+                total += rows - i
+    return total
 
 grid = np.array([list(l.strip()) for l in open(0).read().splitlines()])
+s = time()
+for j in range(grid.shape[1]):
+    north(grid, j)
+print(time() - s)
 
-rows, cols = grid.shape
-while True:
-    changed = False
-    for i in reversed(range(1, rows)):
-        for j in range(cols):
-            if grid[i, j] == 'O' and grid[i-1, j] == '.':
-                grid[i, j] = '.'
-                grid[i-1, j] = 'O'
-                changed = True
-    if not changed:
-        break
-
-total = 0
-for i in range(rows):
-    for j in range(cols):
-        if grid[i, j] == 'O':
-            total += rows - i
-print(total)
+print(count(grid))
