69ae63b98030d1ee216a534f2941c2dd3207dc75
Amey Kusurkar <amey1000@gmail.com>
2025-12-03 11:22:43 +0000

    Improve day 1

--- 2025/day1.rb
+++ 2025/day1.rb
@@ -8,26 +8,24 @@ input.each do |inp|
   dir = inp[0]
   full_amount = inp[1..].to_i
 
-  while full_amount > 0
-    amount = [full_amount, 99].min
+  crossing_zero += full_amount / 100
+  amount = full_amount % 100
 
-    if dir == "L"
-      dest = pointer - amount
+  if dir == "L"
+    dest = pointer - amount
 
-      if pointer > 0 && dest <= 0
-        crossing_zero += 1
-      end
-    else
-      dest = pointer + amount
-
-      if pointer > 0 && dest > 99
-        crossing_zero += 1
-      end
+    if pointer > 0 && dest <= 0
+      crossing_zero += 1
     end
+  else
+    dest = pointer + amount
 
-    pointer = dest % 100
-    full_amount -= amount
+    if pointer > 0 && dest > 99
+      crossing_zero += 1
+    end
   end
+
+  pointer = dest % 100
 
   if pointer == 0
     at_zero += 1
